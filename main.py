5f483231d070f7c3c83a35fbdbe99cdd3fc74b22
Filip <filipandersson.1997.245@gmail.com>
2020-04-28 12:45:00 +0200

    did some final testing

--- main.py
+++ main.py
@@ -17,46 +17,28 @@ from torchvision.models import resnet18, resnet34
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 
+from augmentation import get_train_transforms, get_test_transforms
 
 class Model(pl.LightningModule):
     def __init__(self, hparams):
         super(Model, self).__init__()
         self.hparams = hparams
 
-        self.model = resnet18(pretrained=True)
-        for i, param in enumerate(self.model.parameters()):
-            if i < 12:  # 18
-                param.requires_grad = False
-        self.model.fc = nn.Linear(512, 27)
-        self.model.fc.weight.requires_grad = True
-        self.model.fc.bias.requires_grad = True
+        self.model = torch.hub.load(
+            'rwightman/gen-efficientnet-pytorch', 'mixnet_l', pretrained=True)  # "efficientnet_b0"
+
+        #self.model.classifier = nn.Linear(1280, 24) efficientnet
+        self.model.classifier = nn.Linear(1536, 24)
 
     def prepare_data(self):
         super().prepare_data()
 
-        train_transforms = transforms.Compose([
-            transforms.Pad(224),
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(.3),
-            transforms.ColorJitter(
-                brightness=.12, contrast=.03, saturation=.03),
-            transforms.RandomAffine(5, scale=(.96, 1.06), shear=(5)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[.485, .456, .406],
-                                 std=[.229, .224, .225]),
-            transforms.RandomErasing(p=.2),
-        ])
+        train_transforms = get_train_transforms()
 
         self.train_dataset = ImageFolder(
             "./dataset/train", transform=train_transforms)
 
-        val_transforms = transforms.Compose([
-            transforms.Pad(224),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[.485, .456, .406],
-                                 std=[.229, .224, .225]),
-        ])
+        val_transforms = get_test_transforms()
         self.val_dataset = ImageFolder(
             "./dataset/val", transform=val_transforms)
 
@@ -66,6 +48,8 @@ class Model(pl.LightningModule):
     def training_step(self, batch, batch_nb):
         # REQUIRED
         x, y = batch
+        x = x["image"]
+        #print(x)
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
         tensorboard_logs = {'Train/loss': loss}
@@ -73,7 +57,8 @@ class Model(pl.LightningModule):
 
     def validation_step(self, batch, batch_nb):
         x, y = batch
-
+        x = x["image"]
+        #print(x)
         # implement your own
         out = self(x)
         loss = F.cross_entropy(out, y)
@@ -110,17 +95,19 @@ class Model(pl.LightningModule):
         scheduler = torch.optim.lr_scheduler.OneCycleLR(
             optimizer=optimizer, max_lr=self.hparams["max_lr"], steps_per_epoch=len(self.train_dataset), epochs=5, cycle_momentum=False)
         return [optimizer], [scheduler]
-
+    
+    @pl.data_loader
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=128, shuffle=True, drop_last=True, num_workers=4)
+        return DataLoader(self.train_dataset, batch_size=64, shuffle=True, drop_last=False, pin_memory=True, num_workers=4)
 
+    @pl.data_loader
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=128, drop_last=True, num_workers=4)
+        return DataLoader(self.val_dataset, batch_size=128, drop_last=False, num_workers=4)
 
 
 if __name__ == "__main__":
     model = Model(OrderedDict({
-        "lr": 5e-5,
+        "lr": 5e-4,
         "max_lr": 1e-2,
         "weight_decay": .07
     }))
@@ -141,8 +128,11 @@ if __name__ == "__main__":
     )
 
     trainer = pl.Trainer(
+        
+        amp_level='O1',
+        precision=16,
         gpus=1,
-        auto_lr_find=True,
+        auto_lr_find=False,
         early_stop_callback=early_stop_callback,
         checkpoint_callback=checkpoint_callback,
         check_val_every_n_epoch=1,
